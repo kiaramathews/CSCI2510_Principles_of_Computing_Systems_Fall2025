4f9e9c847f87e475f40237f1e352f65dd1175ea3
Qinglei Cao <qingleicao@Qingleis-MacBook-Pro.local>
2025-10-28 10:47:11 -0500

    Lab2

---
+++ lab-2-slu-shell-slush/slush.c
@@ -0,0 +1,13 @@
+//SLUSH - The SLU SHELL
+//
+//This assignment reads user input, and executes that input as a sequence of
+//programs to execute.
+//
+//Program execution commands have the form:
+//
+//prog_n [args] ( ... prog_3 [args] ( prog_2 [args] ( prog_1 [args]
+//
+//For example, "grep slush ( sort -r ( ls -l -a" returns a reverse sorted list
+//of all files in the current directory that contain the string "slush" in
+//
+//See the lab writeup for more details and other requirements.
